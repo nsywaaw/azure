63e858f393d33f8abded8422381f1298f3b54d46
Nasywa Alif Widyasari <nasywalidyasari@gmail.com>
2024-12-10 21:51:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<? php
+$name = "Tugas Awan Kelompok 4";
+$text = "Ini tugas awan week 12";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equix="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Tugas Awan Week 12</title>
+</head>
+<body>
+    <h1>Komputasi Awan dan Sistem Terdistribusi</h1>
+    <p><?php echo &name ?></p>
+    <p><?php echo &text ?></p>
+</body>
+</html>
